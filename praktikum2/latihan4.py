e52747e68f186c6f58232b60d5b72aab2fdb449a
Annisa Rachmawati <71623132+annisarac@users.noreply.github.com>
2020-11-01 21:40:11 +0700

    Create latihan4.py

---
+++ praktikum2/latihan4.py
@@ -0,0 +1,10 @@
+def luasSegitiga2(a, t):
+    luas = a * t / 2
+    print('Luas segitiga dengan alas' , alas , 'dan tinggi', tinggi , 'adalah', luas)
+    
+alas = 10
+tinggi = 20
+alas1 = 15
+tinggi1 = 45
+luasSegitiga2(alas, tinggi)
+luasSegitiga2(alas1, tinggi1)
