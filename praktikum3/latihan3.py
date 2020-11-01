e9184303abf172a7bf87cb391c7d16e5d79317c2
Annisa Rachmawati <71623132+annisarac@users.noreply.github.com>
2020-11-01 21:41:01 +0700

    Create latihan3.py

---
+++ praktikum3/latihan3.py
@@ -0,0 +1,21 @@
+from operation import *
+
+x = kali(4, 6)
+y = jumlah(2, x)
+z = kurang(y, 4)
+
+print(z)
+
+m = jumlah(4, 7)
+n = kurang (6, 9)
+o = kali(m, n)
+
+print(o)
+
+p = jumlah(10, 2)
+q = jumlah(7, 5)
+r = kurang(12, 34)
+s = bagi(p, q)
+t = bagi(s, r)
+
+print(t)
